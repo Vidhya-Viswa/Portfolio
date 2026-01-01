76aed6a3ea2912a3de36b5e8dab8c24535c6af46
Vidhya-Viswa <vidhyaviswa20@gmail.com>
2026-01-01 16:05:35 +0530

    Add js file

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+            ui: ['framer-motion', 'react-simple-typewriter']
+          }
+        }
+      }
+    }
+  });
+
